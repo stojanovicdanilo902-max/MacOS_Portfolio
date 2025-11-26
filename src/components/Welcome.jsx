ee62299aa0f5c809ed30555726bb45888f2a8071
Danilo Stojanovic <stojanovicdanilo902@gmail.com>
2025-11-26 22:07:08 +0100

    implement coderabbits suggestions

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -21,7 +21,7 @@ const renderText = (text, className, baseWeight = 400) => {
 };
 
 const setupTextHover = (container, type) => {
-    if(!container) return;
+    if(!container) return () => {};
 
     const letters = container.querySelectorAll("span");
     const { min, max, default: base} = FONT_WEIGHTS[type];
@@ -81,7 +81,7 @@ const Welcome = () => {
         </h1>
 
         <div className="small-screen">
-            <p>This portfolio is designed for desktop/tabled screens only.</p>
+            <p>This portfolio is designed for desktop/tablet screens only.</p>
         </div>
     </section>
   )
